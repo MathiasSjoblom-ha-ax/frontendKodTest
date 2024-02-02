a55a619a512e4519ac91bf10fed5e09e52cc6cd4
Mathias Sjöblom <mathias.sjoblom@ha.ax>
2024-02-02 05:43:01 +0200

    added search function and reset function to search the table of content and CSS

--- src/app/api-data-fetch/api-data-fetch.component.ts
+++ src/app/api-data-fetch/api-data-fetch.component.ts
@@ -11,11 +11,16 @@ import { Observable } from 'rxjs';
 export class APIDataFetchComponent implements OnInit {
   //List to store all fund from API
   fundList: Funds[] = [];
+  //Variable to store the current search input
+  searchQuery = '';
+  //Variable to store new data from the search
+  searchResults: Funds[] = [];
 
   httpClient = inject(HttpClient);
 
   ngOnInit(): void {
     this.fetchAPI();
+    this.search();
   }
 
   //Fetches the data from the API and inserts it into fundList
@@ -34,10 +39,33 @@ export class APIDataFetchComponent implements OnInit {
           } as Funds;
         });
         console.log(this.fundList);
+        this.search();
       },
       (error: any) => {
         console.error('API Error: ', error);
       }
     );
   }
+
+  //Function to handle the search input and update the searchResult array
+  search(): void {
+    let query = this.searchQuery;
+    if (query) {
+      this.searchResults = this.fundList.filter(fund => 
+        fund.fundName.toLowerCase().includes(query.toLowerCase()) ||
+        fund.currency.toLowerCase().includes(query.toLowerCase()) ||
+        fund.fundType.toLowerCase().includes(query.toLowerCase())
+      );
+    } else {
+      this.searchResults = this.fundList;
+    }
+  }
+  
+  //Function to reset the search with the reset button
+  resetSearch(event: Event): void {
+    event.preventDefault();
+    this.searchQuery = '';
+    this.search();
+  }
+  
 }
